12504e37ce96cfcb5b4c0df18d3ff49c7d829272
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-03 20:29:55 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// saturdayFun
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`; // Fix: Use backticks for template literals
+}
+console.log(saturdayFun()); 
+
+// mondayWork
+function mondayWork(task = "go to the office") {
+  return `This Monday, I will ${task}.`; // Fix: Use backticks for template literals
+}
+console.log(mondayWork()); 
+
+// wrapAdjective
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`; // Fix: Use backticks for template literals
+  };
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction()); // Output: You are !!!special!!!!
